2d3434de573aeb76949ade093102de9e7cd33971
bartlannoeye <bart.lannoeye@gmail.com>
2015-10-21 14:48:14 +0200

    Moving blog sample to github

---
+++ README.md
@@ -0,0 +1,10 @@
+# Binding updates Sample
+
+This repository contains a sample solution to test how the default TextBox binding updates and how to enable tracking input changes realtime.
+
+Blog post: http://www.bartlannoeye.com/blog/tracking-ongoing-text-changes-in-your-universal-app
+
+####Used libraries:
+
+ * [MVVM Light](https://mvvmlight.codeplex.com/) for RelayCommand (copied class file)
+ * [Prism for Windows Runtime](https://prismwindowsruntime.codeplex.com/) for BindableBase (copied class file)
---
+++ src/BindingUpdates/BindingUpdates.Windows/MainViewModel.cs
@@ -0,0 +1,69 @@
+﻿using System.Diagnostics;
+using Windows.UI.Xaml.Controls;
+using GalaSoft.MvvmLight.Command;
+using Microsoft.Practices.Prism.StoreApps;
+
+namespace BindingUpdates
+{
+    internal class MainViewModel : BindableBase
+    {
+        private int _counter = 0;
+
+        public MainViewModel()
+        {
+            Text1 = "Text 1";
+            Text2 = "Text 2";
+            Text3 = "Text 3";
+
+            TextChangedCommand = new RelayCommand<TextChangedEventArgs>(OnTextChanged);
+        }
+
+        private string _text1;
+        public string Text1
+        {
+            get { return _text1; }
+            set
+            {
+                SetProperty(ref _text1, value);
+                Log("Text 1 updated: " + value);
+            }
+        }
+        private string _text2;
+        public string Text2
+        {
+            get { return _text2; }
+            set
+            {
+                SetProperty(ref _text2, value);
+                Log("Text 2 updated: " + value);
+            }
+        }
+        private string _text3;
+        public string Text3
+        {
+            get { return _text3; }
+            set
+            {
+                SetProperty(ref _text3, value);
+                Log("Text 3 updated: " + value);
+            }
+        }
+
+        public RelayCommand<TextChangedEventArgs> TextChangedCommand { get; private set; }
+
+        private void Log(string txt)
+        {
+            Debug.WriteLine(txt);
+        }
+
+        private void OnTextChanged(TextChangedEventArgs args)
+        {
+            _counter++;
+            if (_counter == 3)
+            {
+                Text1 = "Reset text"; // only works once while editing
+                _counter = 0;
+            }
+        }
+    }
+}
